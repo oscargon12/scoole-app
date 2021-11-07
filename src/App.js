3554aca8ba5d96e763bc5caee61c5644d8ce4330
oscargon12 <oscargon12@gmail.com>
2021-11-07 01:38:16 -0500

    MDesafio 2: mnÃu e-commerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navBar/NavBar' //🔸Importando por default
+// import { NavBar } from './components/NavBar' // Importando SIN default
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <NavBar/> {/* Siempre debo cerrar estos tags */}
+
+      <section>
+      </section>
     </div>
   );
 }
